2d744e64091f1d2627950525a6850849ea646129
kzoeps <kzoepa@gmail.com>
2023-04-11 21:09:58 -0400

    feat: update stuff

---
+++ Assets/Scripts/Barracks/BarrackProduction.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrackProduction : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float spawnInterval = 1f;
+    private float nextSpawnTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+       nextSpawnTime = Time.time + spawnInterval; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       HandleSpawning(); 
+    }
+
+    private void HandleSpawning()
+    {
+        if (Time.time >= nextSpawnTime)
+        {
+            Vector3 spawnPosition = transform.position + Random.insideUnitSphere * 5f;
+            nextSpawnTime = Time.time + spawnInterval;
+            Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
