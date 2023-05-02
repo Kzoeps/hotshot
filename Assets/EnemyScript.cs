6d715819486582d0438e33d5ecb6d69f648d2706
kzoeps <kzoepa@gmail.com>
2023-05-02 18:16:32 -0400

    add ai agent

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -1,21 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyScript : MonoBehaviour
 {
     private int health = 100;
+    public AIPath aiPath;
     public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("Attack", 2f, 3f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       HandleMove(); 
+    }
+
+    private void Attack() {
+        animator.SetTrigger("Attack");
+    }
+
+    private void HandleMove() {
+        if (aiPath.desiredVelocity.x >= 0.01f) {
+            transform.localScale = new Vector3(-1f,1f, 1f);
+            animator.SetInteger("AnimState", 2); 
+        } else if (aiPath.desiredVelocity.x <= -0.01f) {
+            transform.localScale = new Vector3(1f,1f, 1f);
+            animator.SetInteger("AnimState", 2); 
+        }
     }
 
     public void TakeDamage(int amount) {
