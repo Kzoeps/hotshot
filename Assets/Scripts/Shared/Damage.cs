e9da389256bd037c9f0719b525959d10aeb234b2
kzoeps <kzoepa@gmail.com>
2023-05-02 21:07:06 -0400

    base class

---
+++ Assets/Scripts/Shared/Damage.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Damage : MonoBehaviour
+{
+    public int health = 100;
+    public void TakeDamage(int amount) {
+        health -= amount;
+        if (health <= 0) {
+            Die();
+        }
+    };
+
+    public abstract void Die();
+
+
+}
